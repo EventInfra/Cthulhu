9a1bc7bb271533bf3d6ae4d8bee6850f6cdac65e
rewby <rewby@rewby.nl>
2026-08-08 14:34:23 +0000

    Workaround a small bug in arista updates.

--- cthulhu-provision/templates/arista-stage2-os-change.sh
+++ cthulhu-provision/templates/arista-stage2-os-change.sh
@@ -17,8 +17,13 @@ function clean_nonboot_swi() {
 
 function main() {
   echo "[PROVISION-S2-UPGRADE] Entered main."
-  echo -en "%%%%%\"SoftwareUpdatePerformed\"%%%%%\r\n" >> $REALTTY
+  echo -en "%%%%%\"SoftwareUpdatePerformed\"%%%%%\r\n" | sudo tee $REALTTY > /dev/null
   clean_nonboot_swi
+  export SKU="$(awk -F" " '/^Sku: / {print $2}' /etc/fdl)"
+  if [[ "x$SKU" == "xDCS-7150S-"* ]]; then
+    echo "[PROVISION-S2-UPGRADE] Special case: Delete all SWI before update... THIS MAKES THE SWITCH UNBOOTABLE"
+    rm -v /mnt/flash/*.swi
+  fi
   echo "[PROVISION-S2-UPGRADE] Fetching new OS..."
   curl -o "/mnt/flash/$TARGET_SWI" "{{base_url}}/provision/arista/swi/$TARGET_SWI"
   echo "[PROVISION-S2-UPGRADE] Writing out boot config..."
@@ -27,7 +32,7 @@ CONSOLESPEED=9600
 SWI=flash:/$TARGET_SWI
 EOF
   echo "[PROVISION-S2-UPGRADE] Finished OS upgrade!"
-  echo -en "PROVISION_REBOOT\r\n" >> $REALTTY
+  echo -en "PROVISION_REBOOT\r\n" | sudo tee $REALTTY > /dev/null
 }
 
 main "$@"
